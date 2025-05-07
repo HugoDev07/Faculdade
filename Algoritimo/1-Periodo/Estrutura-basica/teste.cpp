d39feba1b249cc20ee265ee6f3b801fdfb478bcc
HugoDev07 <hugosamul5@gmail.com>
2025-05-07 16:29:25 -0300

    modifiquei o arquivo teste

--- Algoritimo/1-Periodo/Estrutura-basica/teste.cpp
+++ Algoritimo/1-Periodo/Estrutura-basica/teste.cpp
@@ -3,14 +3,15 @@ using namespace std;
 int main()
 {
 
-    cout << "programacao e feita com ia insonia e ansiedade" <<endl;
-    cout << "quero ser o mior"<<endl;
-    cout <<"red bull te da asas"<<endl;
-    cout <<"preciso de dinheiro"<<endl;
-    cout <<"queria sair do brasil"<<endl;
-    cout <<"Eu vou arrumar minha irmã para meu mior amigo"<<endl;
-    
-
+    cout << "programacao e feita com ia insonia e ansiedade" << endl;
+    cout << "quero ser o mior" << endl;
+    cout << "red bull te da asas" << endl;
+    cout << "preciso de dinheiro" << endl;
+    cout << "queria sair do brasil" << endl;
+    cout << "quero ser rico" << endl;
+    cout << "ter uma xj6" << endl;
+    cout << "queria ter uma dodge ram " << endl;
+    cout << "ser rico para sustentar a danny";
 
     return 0;
 }
