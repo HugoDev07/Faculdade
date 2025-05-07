9f0a56b7cbd8371847c274f9320e1d9ba582a237
HugoDev07 <hugosamul5@gmail.com>
2025-05-07 15:53:47 -0300

    primeiros codigos

---
+++ Algoritimo/1-Periodo/Estrutura-basica/teste.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "programacao e feita com ia insonia e ansiedade" <<endl;
+    cout << "quero ser o mior"<<endl;
+    cout <<"red bull te da asas"<<endl;
+    cout <<"preciso de dinheiro"<<endl;
+    cout <<"queria sair do brasil"<<endl;
+    
+
+
+    return 0;
+}
